4190115df2b5d242dd77d89fe038e72c4127ae1e
LaetitiaU <laetitia.usai@gmail.com>
2021-02-03 16:17:16 +0100

    formulaire

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function change() {
+	var x = Math.floor(Math.random() * 256); 
+	var y = Math.floor(Math.random() * 256);
+	var z = Math.floor(Math.random() * 256);
+	var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
+	console.log(thergb,document.body.style.background);
+	document.body.style.background=thergb;
+};
+
 const alreadyRegisteredUsernames = ["kim", "emmanuel", "nicolas"];
 
 document.querySelector("form").addEventListener("submit", () => {
@@ -7,12 +16,16 @@ document.querySelector("form").addEventListener("submit", () => {
   document.querySelector("main").innerHTML = "Compte créé :)";
 });
 
+// Pour que les deux mots de passe soient identiques,je me pose la question: quand l'utilisateur tape le MDP dans 
+// confirmation de MDP, la valeur de ce champ doit être la même que celle du MDP.
 
-function change() {
-	var x = Math.floor(Math.random() * 256); 
-	var y = Math.floor(Math.random() * 256);
-	var z = Math.floor(Math.random() * 256);
-	var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
-	console.log(thergb,document.body.style.background);
-	document.body.style.background=thergb;
-}
+const elementConfirmMotDePasse = document.getElementById("confirm-password")
+const elementmotdepasse = document.getElementById("password")
+elementConfirmMotDePasse.addEventListener("input", function(event) {
+  if (elementConfirmMotDePasse.value !== elementmotdepasse.value) {   
+    elementConfirmMotDePasse.setCustomValidity("les deux mots de passes ne sont pas identiques")
+  } else {
+    elementConfirmMotDePasse.setCustomValidity("")
+  }
+})
+
